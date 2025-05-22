80123410b325d51d58cd8adcd4fdf442f7aa36f9
benjaminwhite91 <benjaminwhite91@gmail.com>
2025-05-21 23:29:21 -0400

    Update dummy_test_dag.py

--- dummy_test_dag.py
+++ dummy_test_dag.py
@@ -2,12 +2,16 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from datetime import datetime
 
+default_args = {
+    "start_date": datetime(2023, 1, 1)
+}
+
 with DAG(
     dag_id="test_hello_world",
-    start_date=datetime(2023, 1, 1),
+    default_args=default_args,
     schedule_interval="@daily",
     catchup=False,
-    tags=["test"]
+    tags=["test"],
 ) as dag:
     start = EmptyOperator(task_id="say_hello")
     end = EmptyOperator(task_id="say_goodbye")
